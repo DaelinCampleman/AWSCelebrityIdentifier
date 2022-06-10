f5c069c444a9e4c53db06aa41e9f639753ac9721
Daelin Campleman <u14016673@tuks.co.za>
2022-06-10 16:27:32 +0200

    Removed Keys and ID's

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,11 +1,11 @@
 // Fill in your keys and secrets
 
 const AWSConfig = {
-  region: "us-east-1", // e.g. us-east-1
+  region: "", // e.g. us-east-1
   credentials: {
-    "accessKeyId": "AKIA2M3OUDK2VDUVRGOK",
-    "secretAccessKey": "/iuKS08TExnRlbtv7yU0t2Abd/fQe/ljwWFGIIoG",
-    "region": "us-east-1" // e.g. us-east-1
+    "accessKeyId": "",
+    "secretAccessKey": "",
+    "region": "" // e.g. us-east-1
   }
 };
 
