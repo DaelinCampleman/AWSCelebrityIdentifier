5f4598c4ac41a8740e3ec11f7dca24301d9e3a2f
Daelin Campleman <u14016673@tuks.co.za>
2022-06-10 08:59:19 +0200

    Initial Upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+import { RecognizeCelebritiesCommand, RecognizeCelebritiesCommandInput } from  "@aws-sdk/client-rekognition";
+import * as AWS from "@aws-sdk/client-rekognition";
+import AWSConfig from 'src/aws-exports';
+import { HttpClient } from '@angular/common/http';
+
+const REGION = "us-east-1";
+
+const rekogClient = new AWS.RekognitionClient(AWSConfig);
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "Celebrity Identifier"
+  files: File[] = [];
+  celebName = "";
+
+  constructor(private http: HttpClient){
+
+  }
+
+  public recognize_celebrity = async(params: any) => {
+    try {
+        console.log("Recognizing celebrities...");
+        const response = await rekogClient.send(new RecognizeCelebritiesCommand(params));
+        let name: string | undefined = "";
+        if(response.CelebrityFaces != null){
+          for(let i = 0; i < response.CelebrityFaces.length; i++){
+              name = response.CelebrityFaces[i].Name;
+              break;
+          }
+        }
+
+        this.celebName = name!;
+        return name!;
+      } catch (err) {
+        console.log("Error", err);
+        this.celebName = "There was a problem recognizing the celebrity! Please try again.";
+        return "There was a problem recognizing the celebrity! Please try again.";
+    }
+  }
+
+  public fileChange(event: any){
+    console.log("Running Function");
+    this.celebName = "Identifying...";
+    this.files = [];
+    this.files.push(...event.addedFiles);
+    if(event != null){
+      const reader = new FileReader();
+      const fileByteArray: number[] = [];
+
+      reader.readAsArrayBuffer(event.addedFiles[0]);
+      reader.onloadend = (evt: any) => {
+        if (evt.target.readyState === FileReader.DONE) {
+          const arrayBuffer = evt.target.result,
+            array = new Uint8Array(arrayBuffer);
+          for (const a of array) {
+            fileByteArray.push(a);
+          }
+          console.log("IMage Byte Array: ");
+          console.log(fileByteArray);
+          let result = this.recognize_celebrity({
+            Image: {
+              Bytes: fileByteArray
+            }
+          });
+        }
+      }
+    }
+  }
+
+  onRemove(event: any) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+}
---
+++ src/aws-exports.ts
@@ -0,0 +1,12 @@
+// Fill in your keys and secrets
+
+const AWSConfig = {
+  region: "us-east-1", // e.g. us-east-1
+  credentials: {
+    "accessKeyId": "AKIA2M3OUDK2VDUVRGOK",
+    "secretAccessKey": "/iuKS08TExnRlbtv7yU0t2Abd/fQe/ljwWFGIIoG",
+    "region": "us-east-1" // e.g. us-east-1
+  }
+};
+
+export default AWSConfig;
